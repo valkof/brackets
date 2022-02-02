5c8eedb41ba8092cabc41de31b589c7283b3df8f
valkof <vakofanov@g.bstu.by>
2022-02-03 01:45:35 +0300

    feat: complete task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  str.split('').forEach(bracket => 
+    bracketsConfig.find(twice => twice[0] === stack[stack.length - 1] && twice[1] === bracket) ? stack.pop() : stack.push(bracket)
+  );
+  return !stack.length;
 }
